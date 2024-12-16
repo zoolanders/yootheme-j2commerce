6a5e2d839b73f24d2ad131a62c99f8d422829e40
Miljan <noreply.mn@aleksic.es>
2024-12-16 17:46:47 +0800

    add price and addtocart mapping fields

--- modules/j2store-source/Type/J2StoreProductType.php
+++ modules/j2store-source/Type/J2StoreProductType.php
@@ -22,6 +22,33 @@ class J2StoreProductType
                         'call' => __CLASS__ . '::resolveId',
                     ],
                 ],
+                'price' => [
+                    'type' => 'String',
+                    'metadata' => [
+                        'label' => 'Price',
+                    ],
+                    'extensions' => [
+                        'call' => __CLASS__ . '::resolvePrice',
+                    ],
+                ],
+                'price_raw' => [
+                    'type' => 'String',
+                    'metadata' => [
+                        'label' => 'Price (Raw)',
+                    ],
+                    'extensions' => [
+                        'call' => __CLASS__ . '::resolveRawPrice',
+                    ],
+                ],
+                'addtocart' => [
+                    'type' => 'String',
+                    'metadata' => [
+                        'label' => 'Add to Cart',
+                    ],
+                    'extensions' => [
+                        'call' => __CLASS__ . '::resolveAddToCart',
+                    ],
+                ],
             ],
 
             'metadata' => [
@@ -34,5 +61,31 @@ class J2StoreProductType
     public static function resolveId(J2StoreTableProduct $product): int
     {
         return $product->j2store_product_id;
+    }
+
+    public static function resolvePrice(J2StoreTableProduct $product): string
+    {
+        return \J2Store::product()->displayPrice($product->pricing->base_price, $product);
+    }
+
+    public static function resolveRawPrice(J2StoreTableProduct $product): string
+    {
+        return $product->pricing->price;
+    }
+
+    public static function resolveAddToCart(J2StoreTableProduct $product): string
+    {
+        $html = $product->get_product_cart_html();
+
+        return self::toUIkit($html);
+    }
+
+    protected static function toUIkit(string $html): string
+    {
+        // replace bootstrap class with UIkit
+        $html = str_replace('btn btn-primary', 'uk-button uk-button-primary', $html);
+        $html = str_replace('input-mini form-control', 'uk-input uk-form-small uk-form-width-xsmall', $html);
+
+        return $html;
     }
 }
